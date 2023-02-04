e44ad4fab44eeace41d56138ad33d8dc2c00d869
anzbert <anzbert@gmail.com>
2023-02-04 09:34:45 +0800

    drag and drop working

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,15 @@
+#[derive(Default)]
 pub struct App {
     // label: String,
     // value: f32,
+    dropped_files: Vec<egui::DroppedFile>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {}
-    }
-}
+// impl Default for App {
+//     fn default() -> Self {
+//         Self {}
+//     }
+// }
 
 impl App {
     /// Called once before the first frame.
@@ -22,7 +24,7 @@ impl eframe::App for App {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self {} = self; // deconstructe values from struct if necessary
+        // let Self {} = self; // deconstructe values from struct if necessary
 
         // Examples of how to create different panels and windows.
         // For inspiration and more examples, go to https://emilk.github.io/egui
@@ -39,10 +41,35 @@ impl eframe::App for App {
         //     });
         // });
 
+        // The central panel the region left after adding TopPanel's and SidePanel's
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
             ui.heading("rusty_convertalizer");
+
+            // Show dropped files (if any):
+            if !self.dropped_files.is_empty() {
+                ui.group(|ui| {
+                    ui.label("Dropped files:");
+
+                    for file in &self.dropped_files {
+                        let mut info = if let Some(path) = &file.path {
+                            path.display().to_string()
+                        } else if !file.name.is_empty() {
+                            file.name.clone()
+                        } else {
+                            "???".to_owned()
+                        };
+                        if let Some(bytes) = &file.bytes {
+                            use std::fmt::Write as _;
+                            write!(info, " ({} bytes)", bytes.len()).ok();
+                        }
+                        ui.label(info);
+                    }
+                });
+            }
+
+            // Collect dropped files:
+            let bla = ctx.input().raw.dropped_files.clone();
+            self.dropped_files.extend(bla);
 
             // egui::warn_if_debug_build(ui);
         });
